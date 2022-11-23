eba60484e1969927a569751bc01b407ebdc553ae
Anjan Layek <110993998+AnjanLayek@users.noreply.github.com>
2022-11-23 11:11:30 +0000

    commitiii

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,16 +1,21 @@
-import React from "react"
+import React, { useState } from "react"
 import { useContext } from "react"
 import { UserContext } from "../context/userContext"
 
 export const Settings = () => {
 
     // to be implemented in context
-    const { greeting,setGreeting } = useContext(UserContext)
+    const {greeting,setGreeting} = useContext(UserContext)
+    const [val,setVal] = useState('')
+    const changeHandler =(e)=>{
+        setGreeting(e.target.value);
+        setVal(e.target.value)
+    }
 
     return (
         <div style={{ border: '5px solid red', padding: '8px' }} id="settings">
             <h4>Settings</h4>
-            <input type={'text'} value={greeting} onChange={(e)=>{setGreeting(e.target.value)}}/>
+            <input type={'text'} value={val} onChange={changeHandler}/>
         </div>
     )
 }
